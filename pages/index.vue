05bd867012ccd7a583077e9cde516dd6ade5bbfb
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-12 16:28:17 -0400

    Using layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <header-component></header-component>
+<div>
+  HOLA MUNDO
+</div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/Header";
+import SideBar from "@/components/SideBar";
 export default {
   components: {
-    HeaderComponent
+    HeaderComponent, SideBar
   }
 }
 </script>

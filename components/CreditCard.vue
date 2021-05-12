fee36bd2849de1099683f1b634718cd61a4f5294
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-12 18:55:50 -0400

    Created CerditCard component

---
+++ components/CreditCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="credit-card">
+    <div class="credit-card__header">
+      <p class="credit-card__title">cloudcash</p>
+      <p class="credit-card__info">PREMIUM ACCOUNT</p>
+    </div>
+    <div class="credit-card__number">
+      <ul>
+        <li>4351</li>
+        <li>****</li>
+        <li>****</li>
+        <li>0104</li>
+      </ul>
+    </div>
+    <div class="credit-card__footer">
+      <div>
+        <p class="credit-card__info">Card holder</p>
+        <p class="credit-card__title">Carlos García</p>
+      </div>
+      <div>
+        <p class="credit-card__info">Expire date</p>
+        <p class="credit-card__title">06/21</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>

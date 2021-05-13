0fe1a81f613811c5eb6898ad22745a1a380b1cb1
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 01:57:17 -0400

    Created Table Component

---
+++ components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table">
+    <tr>
+      <th>Receiver</th>
+      <th>Type</th>
+      <th>Date</th>
+      <th>Amount</th>
+    </tr>
+    <tr>
+      <td>Tesco Market</td>
+      <td>Shopping</td>
+      <td>13 DEC 2020</td>
+      <td>$75.67</td>
+    </tr>
+    <tr>
+      <td>Tesco Market</td>
+      <td>Shopping</td>
+      <td>13 DEC 2020</td>
+      <td>$75.67</td>
+    </tr>
+    <tr>
+      <td>Tesco Market</td>
+      <td>Shopping</td>
+      <td>13 DEC 2020</td>
+      <td>$75.67</td>
+    </tr>
+    <tr>
+      <td>Tesco Market</td>
+      <td>Shopping</td>
+      <td>13 DEC 2020</td>
+      <td>$75.67</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {};
+</script>

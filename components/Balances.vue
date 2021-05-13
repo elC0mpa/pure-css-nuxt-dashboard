ab6021493d60b0c7722437ad832ac00bfc4287c9
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 11:07:19 -0400

    Using secondary-info typography

--- components/Balances.vue
+++ components/Balances.vue
@@ -2,15 +2,15 @@
   <div class="balances">
     <div class="balances__current">
       <p class="balances__current-balance current-balance-typo">2000.50</p>
-      <p class="balances__info">Current balance</p>
+      <p class="balances__info secondary-info">Current balance</p>
     </div>
     <div class="balances__income">
       <p class="balances__income-balance income-balance-typo">1000.12</p>
-      <p class="balances__info">Income</p>
+      <p class="balances__info secondary-info">Income</p>
     </div>
     <div class="balances__outcome">
       <p class="balances__outcome-balance outcome-balance-typo">800.43</p>
-      <p class="balances__info">Outcome</p>
+      <p class="balances__info secondary-info">Outcome</p>
     </div>
   </div>
 </template>
--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -4,8 +4,8 @@
       <span style="width: 25%"></span>
     </div>
     <div class="progress-bar__info-container">
-        <p class="progress-bar__info-container--text">Weekly Payment Limit</p>
-        <p class="progress-bar__info-container--status">$350.00/$400.00</p>
+        <p class="progress-bar__info-container--text secondary-info">Weekly Payment Limit</p>
+        <p class="progress-bar__info-container--status secondary-info">$350.00/$400.00</p>
     </div>
   </div>
 </template>

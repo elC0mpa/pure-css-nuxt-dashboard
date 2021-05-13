76f41df66ce0a684632293620d78294b389defdf
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-12 22:29:38 -0400

    Created balances component

---
+++ components/Balances.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="balances">
+    <div class="balances__current">
+      <p class="balances__current-balance current-balance-typo">2000.50</p>
+      <p class="balances__info">Current balance</p>
+    </div>
+    <div class="balances__income">
+      <p class="balances__income-balance income-balance-typo">1000.12</p>
+      <p class="balances__info">Income</p>
+    </div>
+    <div class="balances__outcome">
+      <p class="balances__outcome-balance outcome-balance-typo">800.43</p>
+      <p class="balances__info">Outcome</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>

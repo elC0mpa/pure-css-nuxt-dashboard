a4fd48097e3bea42ba31d687e237b73080bf594e
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 14:11:37 -0400

    GoalsSection created

--- components/GoalsSection.vue
+++ components/GoalsSection.vue
@@ -4,9 +4,19 @@
       <p class="card-title">Goals</p>
       <button class="infinite-button">+</button>
     </div>
+    <div class="goals-section__cards-container">
+      <goal-card></goal-card>
+      <goal-card></goal-card>
+      <goal-card></goal-card>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+import GoalCard from "@/components/GoalCard";
+export default {
+  components: {
+    GoalCard
+  }
+};
 </script>

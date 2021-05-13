961331963802e1a1f974202859d3b8b34dd3859d
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 00:53:14 -0400

    Created SwitchComponent

---
+++ components/Switch.vue
@@ -0,0 +1,17 @@
+<template>
+  <label class="switch">
+    <input v-model="value" type="checkbox" />
+    <span class="slider"></span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "SwitchInput",
+  data() {
+    return {
+      value: false,
+    };
+  },
+};
+</script>

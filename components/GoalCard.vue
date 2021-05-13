c3c4aa8ee4ff263e0db2085ff7d7d753fef82e29
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 14:11:20 -0400

    GoalCard component created

---
+++ components/GoalCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="goal-card">
+    <div class="goal-card__top-container">
+      <p class="goal-card__top-container--balance is-currency">550</p>
+      <p class="goal-card__top-container--date">12/11/19</p>
+    </div>
+    <div class="goal-card__bottom-container">
+      <div class="goal-card__bottom-container--icon" v-html="Graph"></div>
+      <div class="goal-card__bottom-container--id">Holidays</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Graph from "@/assets/icons/area-graph.svg?raw";
+export default {
+  data() {
+    return {
+      Graph,
+    };
+  },
+};
+</script>

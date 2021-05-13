8a6daa69e4c3bdfe1e0ddf59a6f23c7ad032787b
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-12 22:30:21 -0400

    Created CardsSection component

---
+++ components/CardsSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="cards-section">
+    <p class="card-title">Cards</p>
+    <div class="cards-section__top-container">
+      <credit-card></credit-card>
+      <balances></balances>
+    </div>
+  </section>
+</template>
+
+<script>
+import CreditCard from "@/components/CreditCard";
+import Balances from "@/components/Balances"
+export default {
+  components: {
+    CreditCard,
+    Balances
+  },
+};
+</script>

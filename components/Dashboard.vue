f5aa0801f0d7a321b8d0a7aca1b7f0c641c3fc5f
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-13 01:57:54 -0400

    Using TableComponent inside transactionHistorySection

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -1,19 +1,22 @@
 <template>
   <main class="dashboard">
-      <div class="dashboard__first-column">
-          <cards-section></cards-section>
-      </div>
-      <div class="dashboard__second-column">
-          Second Column
-      </div>
+    <div class="dashboard__first-column">
+      <cards-section></cards-section>
+      <transaction-history-section></transaction-history-section>
+    </div>
+    <div class="dashboard__second-column">
+      Second Column
+    </div>
   </main>
 </template>
 
 <script>
 import CardsSection from "@/components/CardsSection";
+import TransactionHistorySection from "@/components/TransactionHistorySection";
 export default {
   components: {
-      CardsSection
-  }
+    CardsSection,
+    TransactionHistorySection,
+  },
 };
 </script>
---
+++ components/TransactionHistorySection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="transaction-history-section">
+    <p class="card-title">Transaction History</p>
+    <table-component></table-component>
+  </section>
+</template>
+
+<script>
+import TableComponent from "@/components/Table";
+export default {
+  components: {
+    TableComponent
+  }
+};
+</script>

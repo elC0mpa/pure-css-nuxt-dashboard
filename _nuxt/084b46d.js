c43c580a54b0e0530e7e9580f2706ccec0390801
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-05-16 05:12:14 -0400

    Updates

---
+++ _nuxt/084b46d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{234:function(n,t,e){"use strict";e.r(t);var o={components:{Dashboard:e(226).default}},l=e(18),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("dashboard")}),[],!1,null,null,null);t.default=component.exports;installComponents(component,{Dashboard:e(226).default})}}]);
